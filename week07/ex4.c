b9123c4b5b00bf67bf7ff47b2b2b83ddb93e058c
Marat Gubaidullin <99412750+eeetaF@users.noreply.github.com>
2022-10-19 15:55:21 +0300

    Create ex4.c

---
+++ week07/ex4.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+
+void myrealloc(void *ptr, size_t new_size) {
+    if (ptr == NULL) {
+        ptr = malloc(new_size);
+    }
+    if (new_size == 0) {
+        free(ptr);
+    }
+    if (new_size > 0) {
+        void *new_ptr = malloc(new_size);
+
+        if (new_size < malloc_usable_size(ptr)) {
+            for (int i = 0 ; i < new_size; i++) {
+                ((char *)new_ptr)[i] = ((char *)ptr)[i];
+            }
+        }
+        else {
+            for (int i = 0 ; i < malloc_usable_size(ptr); i++) {
+                ((char *)new_ptr)[i] = ((char *)ptr)[i];
+            }
+        }
+
+        free(ptr);
+        ptr = new_ptr;
+    }
+}
+
+int main() {
+    int *ptr = malloc(10 * sizeof(int));
+    for (int i = 0; i < 10; i++) {
+        ptr[i] = i;
+        printf("%d ", ptr[i]);
+    }
+    myrealloc(ptr, 5 * sizeof(int));
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", ptr[i]);
+    }
+    free(ptr);
+    return 0;
+}
